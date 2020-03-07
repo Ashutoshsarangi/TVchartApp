990ad4494c24c503e9aeb6d2f852cc6081ef424f
Ashu <ashutosh.softronet@gmail.com>
2020-03-08 00:50:00 +0530

    Added Tabs For Video Channel and Message Chart Application.

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/client'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + 'client/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    // socket.on('disconnect', function(){
-    //     console.log('user disconnected');
-    //   });
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-      });
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  // socket.on('disconnect', function(){
+  //     console.log('user disconnected');
+  //   });
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+  socket.on('video message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('video message', msg);
+  });
+});
 
-  
-http.listen(3000, function(){
+
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
